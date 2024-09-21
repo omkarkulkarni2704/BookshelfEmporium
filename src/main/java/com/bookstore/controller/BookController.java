87db792009e6b89f45ae56943a3836a23d5bdff4
Omkar Kulkarni <89896505+omkarkulkarni2704@users.noreply.github.com>
2024-09-21 10:33:06 +0530

    Update BookController.java

--- src/main/java/com/bookstore/controller/BookController.java
+++ src/main/java/com/bookstore/controller/BookController.java
@@ -1,4 +1,4 @@
-package com.bookstore.controller;
+package com.bookstore.controller;   
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
